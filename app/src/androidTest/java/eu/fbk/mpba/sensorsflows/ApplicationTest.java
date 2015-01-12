cf9ee7f0c25cc529a5b28e41b63d9281a3bede36
AleBat <alebat25@yahoo.it>
2015-01-12 13:20:08 +0100

    Added App Test with Context, adding AutoLikMode
    Added CsvOutput
    Added DataSaver

--- app/src/androidTest/java/eu/fbk/mpba/sensorsflows/ApplicationTest.java
+++ app/src/androidTest/java/eu/fbk/mpba/sensorsflows/ApplicationTest.java
@@ -3,11 +3,17 @@ package eu.fbk.mpba.sensorsflows;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
+import eu.fbk.mpba.sensorsflows.stubs.CsvOutput;
+import eu.fbk.mpba.sensorsflows.stubs.SmartphoneDevice;
+
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
+        FlowsMan<Long, float[]> m = new FlowsMan<Long, float[]>();
+        m.addDevice(new SmartphoneDevice(getContext()));
+        m.addOutput(new CsvOutput());
     }
 }
--- app/src/main/java/eu/fbk/mpba/sensorsflows/SensorImpl.java
+++ app/src/main/java/eu/fbk/mpba/sensorsflows/SensorImpl.java
@@ -1,6 +1,7 @@
 package eu.fbk.mpba.sensorsflows;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import eu.fbk.mpba.sensorsflows.base.ISensor;
 import eu.fbk.mpba.sensorsflows.base.SensorStatus;
@@ -11,36 +12,9 @@ import eu.fbk.mpba.sensorsflows.util.ReadOnlyIterable;
  */
 public abstract class SensorImpl<TimeT, ValueT> implements ISensor<DeviceImpl<TimeT, ValueT>> {
     private boolean _listened = true;
+    private ArrayList<OutputImpl<TimeT, ValueT>> _outputs = new ArrayList<OutputImpl<TimeT, ValueT>>();
     DeviceImpl<TimeT, ValueT> _parent = null;
     SensorStatus _status = SensorStatus.OFF;
-
-    protected void changeStatus(SensorStatus state) {
-        _parent._manager.sensorStateChanged(this, null, _status = state);
-    }
-
-    @Override
-    public SensorStatus getState() {
-        return _status;
-    }
-
-    protected void setParentDevice(DeviceImpl<TimeT, ValueT> parent) {
-        _parent = parent;
-    }
-
-    @Override
-    public DeviceImpl<TimeT, ValueT> getParentDevice() {
-        return _parent;
-    }
-
-    public boolean isListened() {
-        return _listened;
-    }
-
-    public void setListened(boolean listened) {
-        this._listened = listened;
-    }
-
-    private ArrayList<OutputImpl<TimeT, ValueT>> _outputs = new ArrayList<OutputImpl<TimeT, ValueT>>();
 
     void addOutput(OutputImpl<TimeT, ValueT> _output) {
         this._outputs.add(_output);
@@ -50,9 +24,33 @@ public abstract class SensorImpl<TimeT, ValueT> implements ISensor<DeviceImpl<Ti
         return new ReadOnlyIterable<OutputImpl<TimeT, ValueT>>(_outputs.iterator());
     }
 
+    // Managed protected getters setters
+
     protected FlowsMan<TimeT, ValueT> getManager() {
         return getParentDevice()._manager;
     }
+
+    protected void changeStatus(SensorStatus state) {
+        _parent._manager.sensorStateChanged(this, null, _status = state);
+    }
+
+    protected void setParentDevice(DeviceImpl<TimeT, ValueT> parent) {
+        _parent = parent;
+    }
+
+    // Managed Overrides
+
+    @Override
+    public SensorStatus getState() {
+        return _status;
+    }
+
+    @Override
+    public DeviceImpl<TimeT, ValueT> getParentDevice() {
+        return _parent;
+    }
+
+    // Notify methods
 
     public void sensorValue(TimeT time, ValueT value) {
         getManager().sensorValue(this, time, value);
@@ -61,4 +59,18 @@ public abstract class SensorImpl<TimeT, ValueT> implements ISensor<DeviceImpl<Ti
     public void sensorEvent(TimeT time, int type, String message) {
         getManager().sensorEvent(this, time, type, message);
     }
+
+    // Listenage
+
+    public boolean isListened() {
+        return _listened;
+    }
+
+    public void setListened(boolean listened) {
+        this._listened = listened;
+    }
+
+    // To implement
+
+    public abstract List<String> getValuesDescriptors();
 }

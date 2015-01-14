aedda0ad25b411b897a5570958f8c69d271fa670
AleBat <alebat25@yahoo.it>
2015-01-14 12:02:52 +0100

    Dunno Boh

--- app/src/androidTest/java/eu/fbk/mpba/sensorsflows/ApplicationTest.java
+++ app/src/androidTest/java/eu/fbk/mpba/sensorsflows/ApplicationTest.java
@@ -5,15 +5,12 @@ import android.test.InstrumentationTestCase;
 import eu.fbk.mpba.sensorsflows.stubs.CsvOutput;
 import eu.fbk.mpba.sensorsflows.stubs.SmartphoneDevice;
 
-/**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- */
 public class ApplicationTest extends InstrumentationTestCase {
     FlowsMan<Long, float[]> m;
 
     public ApplicationTest() {
         //super(Application.class);
-        // m = new FlowsMan<Long, float[]>();
+        m = new FlowsMan<Long, float[]>();
         m.addDevice(new SmartphoneDevice(getInstrumentation().getContext()));
         m.addOutput(new CsvOutput());
         m.setAutoLinkMode(AutoLinkMode.NTH_TO_NTH);

3f3352686c0da8f786c8b17fdd28ca8d2f45f450
alebat <alebat25@yahoo.it>
2015-07-17 20:26:16 +0200

    Improved Monotonic Timestamp system's names and syntax

--- sensorsflows/src/main/java/eu/fbk/mpba/sensorsflows/base/IMonotonicTimestampReference.java
+++ sensorsflows/src/main/java/eu/fbk/mpba/sensorsflows/base/IMonotonicTimestampReference.java
@@ -4,9 +4,5 @@ package eu.fbk.mpba.sensorsflows.base;
  * Gives support to keep a monotonic timestamp reference
  */
 public interface IMonotonicTimestampReference {
-    // TODO: improve the structure.
-    // Here it is needed only a get, a reset can be internal
-    // or alternatively the get(void) and the reset or a get reference/bootTime
-    void resetMonoTimestamp(long timestamp, long realTimeNanos);
-    long getMonoTimestampNanos(long realTimeNanos);
+    long getMonoUTCNanos(long realTimeNanos);
 }

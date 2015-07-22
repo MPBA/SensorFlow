ee656fd5183798aabf0d4056131ac062bf072c83
AleBat <alebat25@yahoo.it>
2015-07-22 11:15:27 +0200

    Fixed CSV crash

--- plugins/src/main/java/eu/fbk/mpba/sensorsflows/plugins/plugins/outputs/CsvDataSaver.java
+++ plugins/src/main/java/eu/fbk/mpba/sensorsflows/plugins/plugins/outputs/CsvDataSaver.java
@@ -55,6 +55,7 @@ public class CsvDataSaver {
         for (int i = 0; i < _paths.length; i++)
             try {
                 _files[i] = new File(_paths[i]);
+                _files[i].getParentFile().mkdirs();
                 boolean header = !_files[i].exists();
                 _writs[i] = new FileWriter(_files[i], true);
                 _csvCard[i] = headers.get(i).size();

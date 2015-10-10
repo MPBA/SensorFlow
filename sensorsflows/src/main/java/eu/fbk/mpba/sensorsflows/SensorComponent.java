992f1805408fad270ba14d57505753ed743eb17f
AleB <alebat25@yahoo.it>
2015-10-10 17:17:52 +0200

    Litixcom with simulation OK
    Done a lot of things, I am sorry, it is saturday's tea time and I have no more time and I won't log details.
    LitixComWrapper in simulation mode
    easysetup resources are duplicated

--- sensorsflows/src/main/java/eu/fbk/mpba/sensorsflows/SensorComponent.java
+++ sensorsflows/src/main/java/eu/fbk/mpba/sensorsflows/SensorComponent.java
@@ -3,6 +3,7 @@ package eu.fbk.mpba.sensorsflows;
 import java.util.ArrayList;
 import java.util.List;
 
+import eu.fbk.mpba.sensorsflows.base.IMonoTimestampSource;
 import eu.fbk.mpba.sensorsflows.base.ISensor;
 import eu.fbk.mpba.sensorsflows.base.ISensorDataCallback;
 import eu.fbk.mpba.sensorsflows.base.SensorStatus;
@@ -23,6 +24,21 @@ public abstract class SensorComponent<TimeT, ValueT> implements ISensor {
     private int mForwardedMessages = 0;
     private boolean mListened = true;
     protected SensorStatus mStatus = SensorStatus.OFF;
+
+    private static long _bootTime = System.currentTimeMillis() * 1_000_000L - System.nanoTime();
+
+    private static IMonoTimestampSource _time = new IMonoTimestampSource() {
+
+        @Override
+        public long getMonoUTCNanos() {
+            return System.nanoTime() + _bootTime;
+        }
+
+        @Override
+        public long getMonoUTCNanos(long realTimeNanos) {
+            return realTimeNanos + _bootTime;
+        }
+    };
 
     void addOutput(OutputDecorator<TimeT, ValueT> _output) {
         _outputs.add(_output);
@@ -64,6 +80,10 @@ public abstract class SensorComponent<TimeT, ValueT> implements ISensor {
 
     public String getName() {
         return this.getClass().getSimpleName();
+    }
+
+    public IMonoTimestampSource getTime() {
+        return _time;
     }
 
     // Notify methods

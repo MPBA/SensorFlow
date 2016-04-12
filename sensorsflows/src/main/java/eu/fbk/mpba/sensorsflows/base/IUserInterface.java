42cfb6d5935297aabf771bef8cb4928a526f7cff
AleB-zor-ubu <alebat25@yahoo.it>
2016-04-12 15:53:10 +0200

    Added enable/disable output support

--- sensorsflows/src/main/java/eu/fbk/mpba/sensorsflows/base/IUserInterface.java
+++ sensorsflows/src/main/java/eu/fbk/mpba/sensorsflows/base/IUserInterface.java
@@ -16,54 +16,43 @@ public interface IUserInterface<DeviceT, SensorT extends ISensor, OutputT extend
 
     // ITEMS ENUMERATION control part
 
-    public void addDevice(DeviceT device);
+    void addDevice(DeviceT device);
 
-    public Iterable<DeviceT> getDevices();
+    DeviceT getDevice(String name);
 
-    public void addOutput(OutputT output);
+    Iterable<DeviceT> getDevices();
 
-    public Iterable<OutputT> getOutputs();
+    void addOutput(OutputT output);
 
-    public void addLink(SensorT fromSensor, OutputT toOutput);
+    OutputT getOutput(String name);
 
+    Iterable<OutputT> getOutputs();
+
+    void addLink(SensorT fromSensor, OutputT toOutput);
 
     // ITEMS OPERATION control part
 
-// WAS it is not safe that the user does this as the current setup provides a static configuration
-//    public void initializeDevice(DeviceT device);
-//
-//    public void finalize(DeviceT device);
-//
-//    public void initializeDevice(IOutput<TimeT, ValueT> output);
-//
-//    public void finalize(IOutput<TimeT, ValueT> output);
+    void setOutputEnabled(boolean enabled, String name);
 
-    public void switchOn(SensorT sensor);
-
-    public void switchOff(SensorT sensor);
-
-    public boolean isSensorListened(SensorT sensor);
-
-    public void setSensorListened(SensorT sensor, boolean streaming);
-
+    boolean getOutputEnabled(String name);
 
     // ENGINE OPERATION control part
 
-    public void start();
+    void start();
 
-    public void setPaused(boolean streaming);
+    void setPaused(boolean streaming);
 
-    public boolean isPaused();
+    boolean isPaused();
 
-    public void stop();
+    void stop();
 
     void close();
 
-    public void setOnStateChanged(EventCallback<IUserInterface<DeviceT, SensorT, OutputT>, EngineStatus> callback);
+    void setOnStateChanged(EventCallback<IUserInterface<DeviceT, SensorT, OutputT>, EngineStatus> callback);
 
-    public void setOnDeviceStateChanged(EventCallback<DeviceT, DeviceStatus> callback);
+    void setOnDeviceStateChanged(EventCallback<DeviceT, DeviceStatus> callback);
 
-    public void setOnOutputStateChanged(EventCallback<OutputT, OutputStatus> callback);
+    void setOnOutputStateChanged(EventCallback<OutputT, OutputStatus> callback);
 
-    public EngineStatus getStatus();
+    EngineStatus getStatus();
 }
